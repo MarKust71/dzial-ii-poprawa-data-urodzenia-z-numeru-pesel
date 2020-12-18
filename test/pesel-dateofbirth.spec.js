eff50e395e5a1c7e350406a0c43872a270ef27da
Marek Kustosz <markust71@hotmail.com>
2020-12-18 15:11:58 +0100

    fix test dateOfBirth and refactor

--- test/pesel-dateofbirth.spec.js
+++ test/pesel-dateofbirth.spec.js
@@ -2,19 +2,17 @@ const dateOfBirthFromPesel = require('../dateofbirth-from-pesel');
 
 describe('(15 punktów) Date of birth from PESEL', () => {
     [
-        '89090132712',
-        '96092842277',
-        '01300846796',
-        '83092964357',
-        '04310511743',
-        '16312569649',
-        '18230374667',
-        '03301791241',
-    ].map((givenPesel) => {
-        test('date of birth extracted pesel', () => {
-            expect(dateOfBirthFromPesel(givenPesel)).toMatch(
-                /1989-09-01|1996-09-28|2001-10-08|1983-09-29|2004-11-05|2016-11-25|2018-03-03|2003-10-17/
-            );
+        { givenPesel: '89090132712', returnedDateOfBirth: '1989-09-01' },
+        { givenPesel: '96092842277', returnedDateOfBirth: '1996-09-28' },
+        { givenPesel: '01300846796', returnedDateOfBirth: '2001-10-08' },
+        { givenPesel: '83092964357', returnedDateOfBirth: '1983-09-29' },
+        { givenPesel: '04310511743', returnedDateOfBirth: '2004-11-05' },
+        { givenPesel: '16312569649', returnedDateOfBirth: '2016-11-25' },
+        { givenPesel: '18230374667', returnedDateOfBirth: '2018-03-03' },
+        { givenPesel: '03301791241', returnedDateOfBirth: '2003-10-17' },
+    ].map((testData) => {
+        test('date of birth extracted from pesel', () => {
+            expect(dateOfBirthFromPesel(testData.givenPesel)).toBe(testData.returnedDateOfBirth);
         });
     });
 });
